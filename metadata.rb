fddfc434139f621222200b241fdd306f10cbb9d5
Eric Saxby <sax@livinginthepast.org>
2014-12-29 12:33:59 -0800

    0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dev@wanelo.com'
 license 'MIT'
 description 'Common providers for Circonus checks'
 long_description 'Common providers for Circonus checks'
-version '0.1.0'
+version '0.2.0'
 
 depends 'circonus'
