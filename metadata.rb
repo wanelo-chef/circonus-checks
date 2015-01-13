56e322b9861d516dc46600148d5f8002d19c84a9
Eric Saxby <eric@wanelo.com>
2015-01-13 12:02:58 -0800

    0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dev@wanelo.com'
 license 'MIT'
 description 'Common providers for Circonus checks'
 long_description 'Common providers for Circonus checks'
-version '0.2.0'
+version '0.3.0'
 
 depends 'circonus'
